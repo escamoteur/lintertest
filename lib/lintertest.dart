cffa244952dd54513f7634814383072ebcc9d29f
escamoteur <github@burkharts.net>
2020-07-22 19:53:35 +0200

    added pedantic

--- lib/lintertest.dart
+++ lib/lintertest.dart
@@ -5,4 +5,8 @@ class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
   final s = "this should flag a linter error";
+
+  void func() {
+    String t = s != null ? s : 'a';
+  }
 }
